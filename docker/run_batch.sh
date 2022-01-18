c02a376aeda39c96e09c5db34b1784e371ffd000
Brian Testa <testabrian8@gmail.com>
2022-01-18 11:31:18 -0500

    Initial scripts for executing the Docker image

---
+++ docker/run_batch.sh
@@ -0,0 +1,9 @@
+# Sample script showing how to execute batch calls in the eractive session in the docker container
+# Typical runtime script for batch processing; you wil need to change paths to match your local machine and may need to change the port below
+
+#!/bin/bash
+
+# CHANGE ME
+HOME=/home/brian/Workspace/clean_audio/aer_evasion
+
+docker run -p 10000:8888 -v $HOME/pickles:/pickles -v $HOME/data:/data audio $@
---
+++ docker/run_interactive.sh
@@ -0,0 +1,10 @@
+# Sample script showing how to start an interactive session in the docker container
+# Good for debugging; you wil need to change paths to match your local machine and may need to change the port below
+
+#!/bin/bash
+
+# CHANGE ME
+HOME=/home/brian/Workspace/clean_audio/aer_evasion
+
+docker run -p 10000:8888 -it -v $HOME/pickles:/pickles -v $HOME/data:/data audio
+
