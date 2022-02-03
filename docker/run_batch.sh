562ede7c78ffe7b1aa3520a23ed9286eb09b45b7
Brian Testa <testabrian8@gmail.com>
2022-02-03 16:56:17 -0500

    Now mounting scripts and notebooks as volumes.

--- docker/run_batch.sh
+++ docker/run_batch.sh
@@ -6,4 +6,4 @@
 # CHANGE ME
 HOME=/home/brian/Workspace/clean_audio/aer_evasion
 
-docker run -p 10000:8888 -v $HOME/pickles:/pickles -v $HOME/data:/data audio $@
+docker run -p 10000:8888 -v $HOME/pickles:/pickles -v $HOME/data:/data -v $HOME/scripts:/scripts -v $HOME/notebooks:/notebooks audio $@
--- docker/run_interactive.sh
+++ docker/run_interactive.sh
@@ -6,5 +6,5 @@
 # CHANGE ME
 HOME=/home/brian/Workspace/clean_audio/aer_evasion
 
-docker run -p 10000:8888 -it -v $HOME/pickles:/pickles -v $HOME/data:/data audio
+docker run -p 10000:8888 -it -v $HOME/pickles:/pickles -v $HOME/data:/data -v $HOME/scripts:/scripts -v $HOME/notebooks:/notebooks audio
 
